17c6f71ca8c33fd6d0f0bce1d2a041e49f159dd9
fangnx <fangnaxin@gmail.com>
2019-09-13 23:17:16 -0400

    Updated to fix schedule

--- handler.js
+++ handler.js
@@ -4,10 +4,8 @@
  * @author nxxinf
  * @github https://github.com/fangnx
  * @created 2019-09-12 12:58:17
- * @last-modified 2019-09-13 01:08:50
+ * @last-modified 2019-09-13 23:04:43
  */
-
-'use strict';
 
 const request = require('request');
 
@@ -16,11 +14,16 @@ const hosts = [
   'http://reaction-forum.herokuapp.com'
 ];
 
-module.exports.wake = (event, context, callback) => {
+module.exports.wake = (event, context) => {
+  const currTime = new Date();
+  console.log(`Starting waking your sites at ${currTime}`);
   hosts.forEach(function(host) {
     request(host, (err, res, body) => {
+      console.log(`\nWaking up ${host}`);
       console.log('Error: ' + err);
-      console.log('Status Code: ' + res && res.statusCode);
+      if (res) {
+        console.log('Status Code: ' + res.statusCode);
+      }
     });
   });
 };
